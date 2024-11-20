7cadae5ce698e12352e235c61cfdb616de61d127
quy1003 <quy021002@gmail.com>
2024-11-20 13:48:19 +0700

    change minor err

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,6 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
