15d7df01fc765074a65038ad70e09cf02f5aba2e
quy1003 <quy021002@gmail.com>
2024-11-20 17:53:27 +0700

    push code

--- src/config/dbconnect.ts
+++ src/config/dbconnect.ts
@@ -5,9 +5,13 @@ async function connect() {
     await mongoose.connect(process.env.MONGODB_URL, {
     
     });
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log('Connecting Successfully!');
   } catch (ex) {
     console.error('Failed Connect!');
+  }
+  finally{
+    await mongoose.disconnect();
   }
 }
 
