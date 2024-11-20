bfb23975ce7a0a79adb97d13f2130d84ec0ebd0b
quy1003 <quy021002@gmail.com>
2024-11-20 16:37:42 +0700

    env update

--- src/config/dbconnect.ts
+++ src/config/dbconnect.ts
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/book_management', {
+    await mongoose.connect(process.env.MONGODB_URL, {
     
     });
     console.log('Connecting Successfully!');
