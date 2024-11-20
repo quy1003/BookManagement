0b60d45083b2cd3ee1f889f8b6b404948b1c3f02
quy1003 <quy021002@gmail.com>
2024-11-20 17:55:42 +0700

    push code

--- src/config/dbconnect.ts
+++ src/config/dbconnect.ts
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-    
+      serverApi: { version: '1', strict: true, deprecationErrors: true }
     });
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log('Connecting Successfully!');
